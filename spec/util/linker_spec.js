f4f7a36e3d57e3854ceb7fa10eee0eb43a4b178d
Ben Thorner <ben.thorner@bt.com>
2017-09-22 10:09:54 +0100

    Add extra describe to linker spec

--- spec/util/linker_spec.js
+++ spec/util/linker_spec.js
@@ -8,42 +8,44 @@ describe('Linker', function () {
     this.subject = new Linker(this.panel, this.linkSrv)
   })
 
-  describe('when the panel has no links', function () {
-    it('skips assiging the box link', function () {
-      this.subject.call(this.box)
-      expect(this.box.link).toEqual(undefined)
-    })
-  })
-
-  describe('when the panel link is invalid', function () {
-    beforeEach(function () {
-      this.panel.links = []
-      this.panel.linkIndex = 0
+  describe('call', function () {
+    describe('when the panel has no links', function () {
+      it('skips assiging the box link', function () {
+        this.subject.call(this.box)
+        expect(this.box.link).toEqual(undefined)
+      })
     })
 
-    it('skips assiging the box link', function () {
-      this.subject.call(this.box)
-      expect(this.box.link).toEqual(undefined)
-    })
-  })
+    describe('when the panel link is invalid', function () {
+      beforeEach(function () {
+        this.panel.links = []
+        this.panel.linkIndex = 0
+      })
 
-  describe('when the panel has a link', function () {
-    beforeEach(function () {
-      this.panel.links = ['linkInfo']
-      this.panel.linkIndex = 0
-    })
-
-    it('calls the linkSrv with scoped vars', function () {
-      spyOn(this.linkSrv, 'getPanelLinkAnchorInfo')
-      this.subject.call(this.box)
-      var linkFn = this.linkSrv.getPanelLinkAnchorInfo
-      expect(linkFn).toHaveBeenCalledWith('linkInfo', 'vars')
+      it('skips assiging the box link', function () {
+        this.subject.call(this.box)
+        expect(this.box.link).toEqual(undefined)
+      })
     })
 
-    it('assigns the scoped link to the box', function () {
-      spyOn(this.linkSrv, 'getPanelLinkAnchorInfo').and.returnValue('link')
-      this.subject.call(this.box)
-      expect(this.box.link).toEqual('link')
+    describe('when the panel has a link', function () {
+      beforeEach(function () {
+        this.panel.links = ['linkInfo']
+        this.panel.linkIndex = 0
+      })
+
+      it('calls the linkSrv with scoped vars', function () {
+        spyOn(this.linkSrv, 'getPanelLinkAnchorInfo')
+        this.subject.call(this.box)
+        var linkFn = this.linkSrv.getPanelLinkAnchorInfo
+        expect(linkFn).toHaveBeenCalledWith('linkInfo', 'vars')
+      })
+
+      it('assigns the scoped link to the box', function () {
+        spyOn(this.linkSrv, 'getPanelLinkAnchorInfo').and.returnValue('link')
+        this.subject.call(this.box)
+        expect(this.box.link).toEqual('link')
+      })
     })
   })
 })
